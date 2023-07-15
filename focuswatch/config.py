a6898bbc02b65e51ecd8658181576d65373d5f55
Mateusz Sieradzki <35726839+sieradzki@users.noreply.github.com>
2023-07-15 15:17:14 +0200

    Initial structure / configuration

---
+++ focuswatch/config.py
@@ -0,0 +1,26 @@
+import configparser
+import os
+
+CONFIG_DIR_PATH = os.getcwd()
+CONFIG_FILE_PATH = os.path.join(CONFIG_DIR_PATH,"config.ini")
+
+def initialize_config():
+  config = configparser.ConfigParser()
+
+  try:
+    # Set default values
+    config['Database'] = {'location': '../', 'name': 'focuswatch.sqlite'} # temporary location for dev purposes
+    config['General'] = {'watch_interval': 1}
+
+    # Save configuration to a file
+    with open(CONFIG_FILE_PATH, 'w') as config_file:
+      config.write(config_file)
+
+    print("Configuration file initialized successfully.") # Logging module?
+  
+  except Exception as e:
+    print(f" An error occured while initializing the configuration file: {e}")
+
+
+if __name__ == '__main__':
+  initialize_config()
