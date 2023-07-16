e6b6a6cae386fd19e33ebd386cce6ef3c25a172c
Mateusz Sieradzki <35726839+sieradzki@users.noreply.github.com>
2023-07-16 19:21:57 +0200

    database initial
    Created DatabaseManager class with basic functionality
    Added load_config function

--- focuswatch/config.py
+++ focuswatch/config.py
@@ -4,7 +4,6 @@ import os
 CONFIG_DIR_PATH = os.getcwd()
 CONFIG_FILE_PATH = os.path.join(CONFIG_DIR_PATH, "config.ini")
 
-
 def initialize_config():
   if os.path.exists(CONFIG_FILE_PATH):
     return
@@ -14,19 +13,26 @@ def initialize_config():
   try:
     # Set default values
     config['General'] = {'watch_interval': 1}
-    # temporary location for dev purposes
-    config['Database'] = {'location': '../', 'name': 'focuswatch.sqlite'}
+    config['Database'] = {'location': '../', 'name': 'focuswatch.sqlite'} # temporary location for dev purposes
 
     # Save configuration to a file
     with open(CONFIG_FILE_PATH, 'w') as config_file:
       config.write(config_file)
 
-    print("Configuration file initialized successfully.")  # Logging module?
-
+    print("Configuration file initialized successfully.") # Logging module?
+  
   except Exception as e:
-    print(
-        f" An error occured while initializing the configuration file: {e}")
+    print(f" An error occured while initializing the configuration file: {e}")
 
+def load_config():
+  if not os.path.exists(CONFIG_FILE_PATH):
+    print(f"Configuration file {CONFIG_FILE_PATH} not found. Creating default configuration")
+    initialize_config()
+
+  config = configparser.ConfigParser()
+  config.read(CONFIG_FILE_PATH)
+
+  return config
 
 if __name__ == '__main__':
   initialize_config()
---
+++ focuswatch/database.py
@@ -0,0 +1,78 @@
+import sqlite3
+import time
+from config import load_config
+
+
+class DatabaseManager:
+  def __init__(self):
+    self._config = load_config()
+    self._db_name = self._config['Database']['name']
+    self._conn = None
+
+    try:
+      dburi = f"file:{self._db_name}?mode=rw"
+      self._conn = sqlite3.connect(dburi, uri=True)
+    except sqlite3.OperationalError:
+      print("Error connecting to database")
+      print("Creating new database")
+      self._create_db()
+
+    self._cur = self._conn.cursor()
+
+  def __del__(self):
+    self._conn.close()
+
+  def _create_db(self):
+    self._conn = sqlite3.connect(self._db_name)
+    self._cur = self._conn.cursor()
+
+    self._cur.execute('''
+      CREATE TABLE "activity_log" (
+          "time_start"	TEXT NOT NULL,
+          "time_stop" 	TEXT NOT NULL,
+          "duration"      REAL NOT NULL,
+          "window_class"  TEXT NOT NULL,
+          "window_name"	TEXT NOT NULL,
+          "tags"          TEXT
+      );''')
+    self._conn.commit()
+
+    print("Database created successfully")
+
+  def insert_activity(self, window_class, window_name, time_start, time_stop, duration, tags=""):
+    if self._conn is not None:
+      t = (time_start, time_stop, duration, window_class, window_name, tags)
+      self._cur.execute(
+          'INSERT INTO activity_log VALUES (?, ?, ?, ?, ?, ?)', t)
+      if self._conn.commit():
+        return True
+      return False
+
+  def get_all_entries(self):
+    if self._conn is not None:
+      res = self._cur.execute("SELECT * FROM activity_log")
+      if res:
+        return res.fetchall()
+      else:
+        return ""
+
+  def get_todays_entries(self):
+    if self._conn is not None:
+      today = time.strftime("%d-%m",
+                            time.localtime(time.time()))
+      res = self._cur.execute(
+          f"SELECT * FROM 'activity_log' WHERE time_start LIKE '%{today}%'")
+      return res.fetchall()
+
+  def get_weeks_entries(self):
+    if self._conn is not None:
+      res = self._cur.execute(
+          f"SELECT * FROM 'activity_log' WHERE DATETIME(time_start) >= DATETIME('now', 'weekday 0', '-7 days')")
+      return res.fetchall()
+
+
+if __name__ == "__main__":
+  db_object = DatabaseManager()
+  db_object.insert_activity('vscodium', 'VSCodium', '19:11', '19:12', 1, 'programming')
+  entries = db_object.get_all_entries()
+  print(entries)
