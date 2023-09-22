6f4acbdf4838f5f08b520860d69644889acae9eb
Mateusz Sieradzki <35726839+sieradzki@users.noreply.github.com>
2023-09-22 09:52:53 +0200

    Update database.py
    - Insert default categories and keywords on database creation
    - Updated methods to work with new table columns

--- focuswatch/database.py
+++ focuswatch/database.py
@@ -16,6 +16,7 @@ class DatabaseManager:
       print("Error connecting to database")
       print("Creating new database")
       self._create_db()
+      self.insert_default_categories()
 
     self._cur = self._conn.cursor()
 
@@ -43,10 +44,11 @@ class DatabaseManager:
         "id"	INTEGER NOT NULL UNIQUE,
         "name"	TEXT NOT NULL,
         "parent_category"	INTEGER,
-        "color" TEXT NOT NULL,
+        "color" TEXT,
         FOREIGN KEY("parent_category") REFERENCES "categories"("id"),
         PRIMARY KEY("id" AUTOINCREMENT)
       );''')
+    # "ignore_case" INTEGER,
 
     self._cur.execute('''
       CREATE TABLE "keywords" (
@@ -81,13 +83,84 @@ class DatabaseManager:
 
     print("Database created successfully")
 
+  def insert_default_categories(self):
+    """ Default categories based on activity watch """
+    self.create_category("Work", None, "#00cc00")
+    self.create_category("Programming", 1)
+    self.create_category("Documents", 1)
+    self.create_category("Image", 1)
+    self.create_category("Audio", 1)
+    self.create_category("3D", 1)
+
+    self.create_category("Comms", None, "#33ccff")
+    self.create_category("IM", 7)
+    self.create_category("Email", 7)
+
+    self.create_category("Media", None, "#ff0000")
+    self.create_category("Games", 10)
+    self.create_category("Video", 10)
+    self.create_category("Social media", 10)
+    self.create_category("Music", 10)
+
+    self.create_category("Uncategorized", None, "#8c8c8c")
+
+    self.insert_default_keywords()
+
+  def insert_default_keywords(self):
+    """ Default keywords based on activity watch """
+    self.add_keyword("Google Docs", 1)
+    self.add_keyword("libreoffice", 1)
+
+    self.add_keyword("GitHub", 2)
+    self.add_keyword("Stack Overflow", 2)
+    self.add_keyword("BitBucket", 2)
+    self.add_keyword("Gitlab", 2)
+    self.add_keyword("vim", 2)
+    self.add_keyword("Spyder", 2)
+    self.add_keyword("kate", 2)
+    self.add_keyword("Visual Studio", 2)
+    self.add_keyword("code", 2)
+
+    self.add_keyword("Gimp", 4)
+    self.add_keyword("Inkscape", 4)
+
+    self.add_keyword("Audacity", 5)
+
+    self.add_keyword("Blender", 6)
+
+    self.add_keyword("Messenger", 8)
+    self.add_keyword("Signal", 8)
+    self.add_keyword("WhatsApp", 8)
+    self.add_keyword("Slack", 8)
+    self.add_keyword("Discord", 8)
+
+    self.add_keyword("Gmail", 9)
+    self.add_keyword("Thunderbird", 9)
+    self.add_keyword("mutt", 9)
+    self.add_keyword("alpine", 9)
+
+    self.add_keyword("Minecraft", 11)
+    self.add_keyword("Steam", 11)
+
+    self.add_keyword("YouTube", 12)
+    self.add_keyword("mpv", 12)
+    self.add_keyword("VLC", 12)
+    self.add_keyword("Twitch", 12)
+
+    self.add_keyword("reddit", 13)
+    self.add_keyword("Facebook", 13)
+    self.add_keyword("Instagram", 13)
+
+    self.add_keyword("Spotify", 14)
+
   """ Activities """
 
-  def insert_activity(self, window_class, window_name, time_start, time_stop, category):
+  def insert_activity(self, window_class, window_name, time_start, time_stop, category, project_id):
     if self._conn is not None:
-      t = (time_start, time_stop, window_class, window_name, category)
+      t = (time_start, time_stop, window_class,
+           window_name, category, project_id)
       self._cur.execute(
-          'INSERT INTO activity_log VALUES (?, ?, ?, ?, ?)', t)
+          'INSERT INTO activity_log VALUES (?, ?, ?, ?, ?, ?)', t)
       self._conn.commit()
       if self._conn.total_changes > 0:
         return True
@@ -121,15 +194,17 @@ class DatabaseManager:
 
   """ Categories """
 
-  def create_category(self, category_name, parent_category=None):
+  def create_category(self, category_name, parent_category=None, color=None):
     if self._conn is not None:
-      t = (category_name, parent_category)
+      t = (category_name, parent_category, color)
       self._cur.execute(
-        'INSERT INTO categories (name, parent_category) VALUES (?, ?)', t)
+        'INSERT INTO categories (name, parent_category, color) VALUES (?, ?, ?)', t)
       self._conn.commit()
       if self._conn.total_changes > 0:
         return True
     return False
+
+    # TODO Reclassify every entry ?
 
   def get_all_categories(self):
     if self._conn is not None:
