95034f65cfaed6cef9cba21f7765fa3fbbd9fd91
Mateusz Sieradzki <35726839+sieradzki@users.noreply.github.com>
2024-10-31 11:59:50 +0100

    Update settings_viewmodel.py
    * added some type hints, docstrings and checks

--- focuswatch/viewmodels/settings_viewmodel.py
+++ focuswatch/viewmodels/settings_viewmodel.py
@@ -1,21 +1,27 @@
+import logging
+from typing import Optional
+
 from focuswatch.autostart_manager import (add_to_autostart, is_frozen,
                                           is_in_autostart,
                                           remove_from_autostart)
 from focuswatch.config import Config
 from focuswatch.viewmodels.base_viewmodel import BaseViewModel
 
+logger = logging.getLogger(__name__)
+
 
 class SettingsViewModel(BaseViewModel):
+
   def __init__(self):
     super().__init__()
-    self.config = Config()
-    self._watch_interval = float(
-        self.config.get_value('General', 'watch_interval'))
-    self._watch_afk = self.config.get_value(
-        'General', 'watch_afk') == 'True'
-    self._afk_timeout = float(
-        self.config.get_value('General', 'afk_timeout'))
-    self._autostart_enabled = is_in_autostart()
+    self.config: Config = Config()
+    self._watch_interval: float = float(
+      self.config.get_value('General', 'watch_interval'))
+    self._watch_afk: bool = self.config.get_value(
+      'General', 'watch_afk') == 'True'
+    self._afk_timeout: float = float(
+      self.config.get_value('General', 'afk_timeout'))
+    self._autostart_enabled: bool = is_in_autostart()
 
   @property
   def watch_interval(self) -> float:
@@ -23,6 +29,9 @@ class SettingsViewModel(BaseViewModel):
 
   @watch_interval.setter
   def watch_interval(self, value: float):
+    if value <= 0:
+      logger.warning("Attempted to set a non-positive watch interval.")
+      return
     self._set_property('_watch_interval', value)
 
   @property
@@ -39,6 +48,9 @@ class SettingsViewModel(BaseViewModel):
 
   @afk_timeout.setter
   def afk_timeout(self, value: float):
+    if value < 0:
+      logger.warning("Attempted to set a negative AFK timeout.")
+      return
     self._set_property('_afk_timeout', value)
 
   @property
@@ -50,6 +62,11 @@ class SettingsViewModel(BaseViewModel):
     self._set_property('_autostart_enabled', value)
 
   def is_autostart_available(self) -> bool:
+    """ Check if autostart is available based on the application's state.
+
+      Returns:
+        True if autostart is available; False otherwise.
+    """
     return is_frozen()
 
   def apply_settings(self):
